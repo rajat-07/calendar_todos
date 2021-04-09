4df4e3a7e7bf9df0adf675599d9d02e2505c2bc5
Rajat Parab <rajatparab07@gmail.com>
2021-04-09 20:59:11 +0530

    ddddd ..

--- server.js
+++ server.js
@@ -85,12 +85,18 @@ app.use('/api', routes);
 
 if(process.env.NODE_ENV === 'production'){
     //set static folder
+    // app.use(express.static('client/build'));
+
     app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
 }
 
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('*',(req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 //// deployment stuff ////
 
